bf7c09b026ab3d6be75d5869910ec128e6d6a3c2
Documenter.jl <documenter@juliadocs.github.io>
2024-09-18 12:08:13 +0000

    build based on 0796516

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CornerPlotting","category":"page"},{"location":"#CornerPlotting","page":"Home","title":"CornerPlotting","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CornerPlotting.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CornerPlotting]","category":"page"}]
+}
